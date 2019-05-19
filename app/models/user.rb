faceb81e12f90f8c9810912e43dc77df20631119
ozy <ozyoz614@yahoo.com>
2019-05-19 01:19:33 -0400

    add association between users and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
+
+
 class User < ApplicationRecord
 
+  has_many :articles
+  before_save { self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: { case_sensitive: false},
             length: {minimum: 3, maximum: 25}
@@ -8,6 +12,4 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 105},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
-
-
 end
